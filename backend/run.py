60cfc899b4f439e6de25cffba8099dc5b4978df4
Meghana K R <meghanakr2004@gmail.com>
2026-07-01 18:08:39 +0530

    Add Dockerfile for Hugging Face deployment

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
+
 from app.main import create_app
 import os
 
@@ -7,5 +8,7 @@ print("Looking for model at:", os.path.abspath("models/ct_model.pth"))
 print("File exists:", os.path.exists("models/ct_model.pth"))
 
 app = create_app()
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(debug=False, host='0.0.0.0', port=port)
